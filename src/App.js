956a67d1c1fa52bf2d540f5b0bbfe7705e67d2ba
Navneet kumar Singh <48136007+navsingh3211@users.noreply.github.com>
2023-08-22 21:51:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./styles.css";
 
 export default function App() {
   const [advice, SetAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
+    SetAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
   return (
     <div className="App">
       <h1>{advice}</h1>
+      <button onClick={getAdvice}>buttion</button>
+      <p>
+        You have read <strong>{count}</strong> piece of advices.
+      </p>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
